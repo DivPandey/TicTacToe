97ea8f809c6982e0ffe7154e9c85f39565dd9263
Divyanshu Pandey <divyanshu1106@gmail.com>
2024-04-24 22:56:26 +0530

    Completed with the functionalities of model directory

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -8,4 +8,76 @@ public class Game {
     private Player winner;
     private int nextPlayerMoveIndex;
 
+    public Game(int dimension, List<Player> players) {
+        this.board = new Board(dimension);
+        this.players = players;
+        this.moves = new ArrayList<>();
+        this.gameState = GameState.IN_PROGRESS;
+        this.winner = null;
+        this.nextPlayerMoveIndex = 0;
+
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public List<Move> getMoves() {
+        return moves;
+    }
+
+    public void setMoves(List<Move> moves) {
+        this.moves = moves;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(GameState gameState) {
+        this.gameState = gameState;
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
+
+    public void setWinner(Player winner) {
+        this.winner = winner;
+    }
+
+    public int getNextPlayerMoveIndex() {
+        return nextPlayerMoveIndex;
+    }
+
+    public void setNextPlayerMoveIndex(int nextPlayerMoveIndex) {
+        this.nextPlayerMoveIndex = nextPlayerMoveIndex;
+    }
+
+    public void printBoard() {
+        this.board.printBoard();
+    }
+
+    private boolean validateMove(Move move) {
+        int row = move.getCell().getRow();
+        int col = move.getCell().getCol();
+
+        if (row < 0 || row >= board.getSize() || col < 0  || col >= board.getSize()) {
+            return false;
+        }
+
+        return board.getBoard().get(row).get(col).getCellState().equals(CellState.EMPTY);
+    }
 }
